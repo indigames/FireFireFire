248c317872221d7928591c4432318f584c514387
Duc Pham <duc.pham@indigames.net>
2021-03-11 09:21:41 +0700

    + Add script to enable UI

---
+++ Assets/EnableAll.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableAll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (var i = 0; i < transform.childCount; i++)
+            transform.GetChild(i).gameObject.SetActive(true);
+    }
+}
