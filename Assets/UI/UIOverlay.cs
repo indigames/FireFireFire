0e0304ca89e58f87f525055cd7fb73544d6dd844
ducpham-indi <duc.pham@indigames.net>
2021-04-14 15:01:48 +0700

    - Fix misc issues

--- Assets/UI/UIOverlay.cs
+++ Assets/UI/UIOverlay.cs
@@ -7,21 +7,33 @@ public class UIOverlay : MonoBehaviour
 
     public Gameplay gameplay;
     public Animator animator;
+
+    bool firstTime = true;
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
         gameplay.callbackRestart += Show;
         gameplay.callbackWaitForConfirmStart += Hide;
+
+        yield return new WaitForSeconds(0.2f);
+        Hide();
     }
 
     void Show()
     {
+        if (firstTime)
+        {
+            firstTime = false;
+            return;
+        }
+
         gameObject.SetActive(true);
+
         animator.Play("Show", 0, 0);
     }
 
     void Hide()
     {
-        animator.Play("Hide");
+        animator.Play("Hide", 0, 0);
     }
 }
